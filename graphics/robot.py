22802d3ba8c548dba7045f513a02a56f6b0ccf9c
The Hurt Doctor <akacrasher@akacrasher.com>
2019-06-12 20:22:33 -0600

    updates

--- graphics/robot.py
+++ graphics/robot.py
@@ -4,7 +4,7 @@ from button import Button
 win = GraphWin("My Robot Window", 1200, 800)
 win.setBackground(color_rgb(100, 100, 100))
 
-# Robot (its just a shape right now)
+# Robot
 robot = Image(Point(400, 400), "bb8.png")
 robot.draw(win)
 
@@ -12,15 +12,15 @@ robot.draw(win)
 pwrLight = Circle(Point(280, 30), 20)
 pwrLight.setFill(color_rgb(255, 0, 0))
 pwrLight.draw(win)
-engineLight = Circle(Point(480, 30), 20)
-engineLight.setFill(color_rgb(255, 0, 0))
-engineLight.draw(win)
+motorLight = Circle(Point(480, 30), 20)
+motorLight.setFill(color_rgb(255, 0, 0))
+motorLight.draw(win)
 
 # Buttons
 pwrButton = Button(win, Point(200,30), 100, 30, "Power On/Off")
 pwrButton.activate()
-engineButton = Button(win, Point(380,30), 100, 30, "Motor On/Off")
-engineButton.activate()
+motorButton = Button(win, Point(380,30), 100, 30, "Motor On/Off")
+motorButton.activate()
 mvrtButton = Button(win, Point(650,65), 50, 30, "Right")
 mvltButton = Button(win, Point(550,65), 50, 30, "Left")
 mvupButton = Button(win, Point(600,30), 50, 30, "Up")
@@ -31,7 +31,7 @@ quitButton.activate()
 # Event loop
 pt = win.getMouse()
 pwrOn = False
-engineOn = False
+motorOn = False
 while not quitButton.clicked(pt):
     if pwrButton.clicked(pt) and pwrOn == False:
         pwrLight.setFill(color_rgb(0, 255, 0))
@@ -39,27 +39,27 @@ while not quitButton.clicked(pt):
     elif pwrButton.clicked(pt) and pwrOn == True:
         pwrLight.setFill(color_rgb(255, 0, 0))
         pwrOn = False
-    elif engineButton.clicked(pt) and engineOn == False:
-        engineLight.setFill(color_rgb(0, 255, 0))
+    elif motorButton.clicked(pt) and motorOn == False:
+        motorLight.setFill(color_rgb(0, 255, 0))
         mvrtButton.activate()
         mvltButton.activate()
         mvupButton.activate()
         mvdnButton.activate()
-        engineOn = True
-    elif engineButton.clicked(pt) and engineOn == True:
-        engineLight.setFill(color_rgb(255, 0, 0))
+        motorOn = True
+    elif motorButton.clicked(pt) and motorOn == True:
+        motorLight.setFill(color_rgb(255, 0, 0))
         mvrtButton.deactivate()
         mvltButton.deactivate()
         mvupButton.deactivate()
         mvdnButton.deactivate()
-        engineOn = False
-    elif mvrtButton.clicked(pt) and engineOn == True:
+        motorOn = False
+    elif mvrtButton.clicked(pt) and motorOn == True:
         robot.move(10,0)
-    elif mvltButton.clicked(pt) and engineOn == True:
+    elif mvltButton.clicked(pt) and motorOn == True:
         robot.move(-10,0)
-    elif mvdnButton.clicked(pt) and engineOn == True:
+    elif mvdnButton.clicked(pt) and motorOn == True:
         robot.move(0,10)
-    elif mvupButton.clicked(pt) and engineOn == True:
+    elif mvupButton.clicked(pt) and motorOn == True:
         robot.move(0,-10)
     pt = win.getMouse()
 
